a82d785ed3acd6733b96973683dc2eefa940f6fc
Taofeek Abiodun Olalere <olaleretaofeek@live.com>
2022-03-28 14:52:59 +0100

    Converted: class components to functional, using useContext hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className='book-list'
-        style={{ background: theme.bg, color: theme.syntax }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>First Book</li>
-          <li style={{ background: theme.ui }}>Second Book</li>
-          <li style={{ background: theme.ui }}>Third Book</li>
-        </ul>
-      </div>
-    );
-  }
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className='book-list'
+      style={{ background: theme.bg, color: theme.syntax }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>First Book</li>
+        <li style={{ background: theme.ui }}>Second Book</li>
+        <li style={{ background: theme.ui }}>Third Book</li>
+      </ul>
+    </div>
+  );
 }
 
 export default BookList;
